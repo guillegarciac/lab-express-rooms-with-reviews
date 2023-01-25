29301c893624f6e743940d422c7a5336bbd34685
guille garcia <ggarciacardiel@gmail.com>
2023-01-25 18:52:13 +0100

    adds username to index after log in

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  const user = req.session.currentUser;
+  res.render("index", { title: 'Hello', user });
 });
 
 module.exports = router;
