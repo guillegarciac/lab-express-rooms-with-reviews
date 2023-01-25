0b5877a96851ba6d57cb3bd338f3d69ba64cf132
guille garcia <ggarciacardiel@gmail.com>
2023-01-25 17:44:49 +0100

    updates views, creates roomDetail + removes middleware to check room creation form and routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -63,7 +63,7 @@ router.post('/login', async (req, res, next) => {
       const passwordMatch = await bcrypt.compare(password, userInDB.hashedPassword);
       if (passwordMatch) {
         req.session.currentUser = userInDB;
-        res.render('profile', {userInDB});
+        res.render('roomView', {userInDB});
       } else {
         res.render('auth/login',  { error: 'Unable to authenticate user' });
         return;
--- routes/rooms.routes.js
+++ routes/rooms.routes.js
@@ -14,4 +14,36 @@ router.get('/', async function (req, res, next) {
   }
 });
 
+/* GET form view */
+/* ROUTE /rooms/new */
+router.get('/new', function (req, res, next) {
+  res.render('newRoom');
+});
+
+/* POST get users show inputs */
+/* ROUTE /rooms/new */
+router.post('/new', async function (req, res, next) {
+  const { name, description, imageUrl } = req.body;
+  try {
+    const createdRoom = await Room.create({ name, description, imageUrl });
+    res.redirect(`/rooms/${createdRoom._id}`);
+  } catch (error) {
+    next(error)
+  }
+});
+
+/* GET one room */
+/* ROUTE /rooms/:roomId */
+router.get('/:roomId', async function (req, res, next) {
+  const { roomId } = req.params;
+  const user = req.session.currentUser;
+  try {
+    const room = await Room.findById(roomId);
+    res.render('roomDetail', { room, user });
+  } catch (error) {
+    next(error)
+  }
+});
+
+
 module.exports = router;
