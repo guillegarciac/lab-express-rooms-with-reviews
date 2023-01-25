f078df3cb16015f94c8460200b83c2e7a97608ee
guille garcia <ggarciacardiel@gmail.com>
2023-01-25 13:15:37 +0100

    adds room model

---
+++ models/Room.model.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require("mongoose");
+
+const roomSchema = new Schema(
+  {
+    name: { type: String },
+    description: { type: String },
+    imageUrl: { type: String },
+    owner: { type: Schema.Types.ObjectId, ref: "User" },
+    reviews: []
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Room = model("Room", roomSchema);
+
+module.exports = Room;
